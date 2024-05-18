45139912a6e840efece46683209fba522c706369
AndrejKlukaa <andrej.kljuka@gmail.com>
2024-05-18 16:55:11 +0300

    feat: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
     app.use(cookieParser());
+
+    const corsOptions: CorsOptions = {
+        origin: 'http://localhost:4200',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    };
+
+    app.enableCors(corsOptions);
     await app.listen(3000);
 }
 bootstrap();
